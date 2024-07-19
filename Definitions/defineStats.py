0e1da83c79ad3adb957245c12241ae2308e10b78
brenden burrus <brenden.burrus@gmail.com>
2024-07-19 15:37:05 -0500

    creating ev calculation function

--- Definitions/defineStats.py
+++ Definitions/defineStats.py
@@ -1,13 +1,73 @@
-def average_value(rarity: str, printing: str, exclude_hyperfoil: bool, exclude_regular: bool, market_value_df) -> float:
-    filtered_df = market_value_df[market_value_df['Rarity'] == rarity]
+def average_value(rarity: list, printing: str, exclude_hyperspace: bool, exclude_regular: bool, market_value_df) -> float:
+    filtered_df = market_value_df[market_value_df['Rarity'].isin(rarity)]
 
-    if exclude_hyperfoil:
+    if exclude_hyperspace:
         filtered_df = filtered_df[~filtered_df['Product Name'].str.contains("(Hyperspace)")]
     elif exclude_regular:
         filtered_df = filtered_df[filtered_df['Product Name'].str.contains("(Hyperspace)")]
 
     filtered_df = filtered_df[filtered_df['Printing'] == printing]
-
+    
     average_price = filtered_df["Market Price"].astype(float).sum() / len(filtered_df.index)
 
     return average_price
+
+
+def get_expected_value(num_packs: int, pull_rates: dict, market_value_df):
+    ev = 0
+
+    base_common_avg = average_value(["Common"], "Normal", True, False, market_value_df)
+    base_uc_avg = average_value(["Uncommon"], "Normal", True, False, market_value_df)
+    base_rare_avg = average_value(["Rare"], "Normal", True, False, market_value_df)
+    base_legendary_avg = average_value(["Legendary"], "Normal", True, False, market_value_df)
+
+    hyper_c_uc_avg = average_value(["Common", "Uncommon"], "Normal", False, True, market_value_df)
+    hyper_r_l_avg = average_value(["Rare", "Legendary"], "Normal", False, True, market_value_df)
+
+    foil_c_uc_avg = average_value(["Common", "Uncommon"], "Foil", True, False, market_value_df)
+    hyperfoil_r_l_avg = average_value(["Rare", "Legendary"], "Foil", False, True, market_value_df)
+
+    showcase_avg = average_value(["Showcase"], "Foil", True, False, market_value_df)
+
+    print(f"base common avg = {base_common_avg}")
+    print(f"base uncommon avg = {base_uc_avg}")
+    print(f"base rare avg = {base_rare_avg}")
+    print(f"base lego avg = {base_legendary_avg}")
+
+    print(f"hyper common/uncommon avg = {hyper_c_uc_avg}")
+    print(f"hyper rare/lego avg = {hyper_r_l_avg}")
+
+    print(f"foil c/uc avg = {foil_c_uc_avg}")
+    print(f"hyperfoil r/l avg = {hyperfoil_r_l_avg}")
+    print(f"showcase avg = {showcase_avg}")
+
+    averages_per_pack = {}
+
+    for key, value in pull_rates.items():
+        print(f"{key}, {value}")
+        match key:
+            case "Common Pull Rate":
+                averages_per_pack['Common'] = (base_common_avg * value)
+            case "Uncommon Pull Rate":
+                averages_per_pack['Uncommon'] = (base_uc_avg * value)
+            case "Rare Pull Rate":
+                averages_per_pack['Rare'] = (base_rare_avg * value)
+            case "Legendary Pull Rate":
+                averages_per_pack['Lego'] = (base_legendary_avg * value)
+            case "Hyperspace C/UC Pull Rate":
+                averages_per_pack['Hyper C/UC'] = (hyper_c_uc_avg * value)
+            case "Hyperspace R/L Pull Rate":
+                averages_per_pack['Hyper R/L'] = (hyper_r_l_avg * value)
+            case "Foil Pull Rate":
+                averages_per_pack['Foil'] = (foil_c_uc_avg * value)
+            case "Hyperfoil R/L Pull Rate":
+                averages_per_pack['Hyperfoil R/L'] = (hyperfoil_r_l_avg * value)
+            case "Showcase Pull Rate":
+                averages_per_pack['Showcase'] = (showcase_avg * value)
+
+
+    for key in averages_per_pack:
+        print(f"{key}: {averages_per_pack[key]} per pack. {(averages_per_pack[key] * num_packs)} per box")
+        ev += (averages_per_pack[key] * num_packs)
+
+    print(f"Expected Value of a box is {ev}")
--- DisplayStats.py
+++ DisplayStats.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-from Definitions.defineStats import average_value
+from Definitions.defineStats import average_value, get_expected_value
+from Definitions.definePullRates import get_pull_rates
 
 # List of Showcases
-showcases = [
+leaders = [
     "Gar Saxon - Viceroy of Mandalore", 
     "Qi'ra - I Alone Survived", 
     "Finn - This is a Rescue", 
@@ -22,17 +23,23 @@ showcases = [
     "Lando Calrissian - With Impeccable Taste"
 ]
 
-def cleanse_market_value_csv(csv_name, showcase_list):
+def cleanse_market_value_csv(csv_name, leaders):
 
     # Read Market Data CSV
     df = pd.read_csv(csv_name)
 
+    hyper_leaders = []
+    for leader in leaders:
+        hyper_leaders.append(leader + " (Hyperspace)")
+
     #Cleaning up Market data to remove bases and add the showcase printing option
-    showcase_filter = df['Product Name'].isin(showcase_list)
+    showcase_filter = df['Product Name'].isin(leaders)
     base_filter = df['Product Name'].str.contains("//")
+    hyper_leader_filter = df['Product Name'].isin(hyper_leaders)
 
     df = df[~showcase_filter]
     df = df[~base_filter]
+    df = df[~hyper_leader_filter]
 
     df['Market Price'] = df['Market Price'].map(lambda x: x.replace('$',''))
 
@@ -43,5 +50,6 @@ def cleanse_market_value_csv(csv_name, showcase_list):
 
 if __name__ == "__main__":
 
-    mv_df = cleanse_market_value_csv('market_values.csv', showcases)
-    print(average_value("Legendary", "Normal",True, False, mv_df))
+    mv_df = cleanse_market_value_csv('market_values.csv', leaders)
+    pull_rates = get_pull_rates()
+    get_expected_value(24, pull_rates, mv_df)
